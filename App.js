f65c9c1a7b299f323653cb35116b55070867cc19
gneto <glaudistonson@gmail.com>
2021-04-06 17:12:51 -0300

    animating satars

--- App.js
+++ App.js
@@ -1,12 +1,66 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TouchableWithoutFeedback, View, Animated, Easing } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 
+const numStars = 5;
+
 export default class App extends React.Component {
+  state = {
+    rating: 1,
+    animation: new Animated.Value(1),
+  };
+
+  
+  rate = star => {
+    this.setState({ rating: star })
+  }
+  
+  animate = () => {
+    Animated.timing(this.state.animation, {
+      toValue: 2,
+      duration: 400,
+      easing: Easing.ease,
+      useNativeDriver: true
+    }).start(() => {
+      this.state.animation.setValue(1);
+    })
+  }
   render() {
+    let stars = [];
+
+    const animateScale = this.state.animation.interpolate({
+      inputRange: [1, 1.5, 2],
+      outputRange: [1, 1.4, 1]
+    });
+
+    const animateOpacity = this.state.animation.interpolate({
+      inputRange: [1, 1.2, 2],
+      outputRange: [1, 0.5, 1]
+    })
+
+    const animationStyle = {
+      transform: [{ scale: animateScale }]
+    }
+
+    for (let x = 1; x <= numStars; x++) {
+      stars.push(
+        <TouchableWithoutFeedback
+          key={x}
+          onPress={() => {
+            this.rate(x), this.animate();
+          }}
+        >
+          <Animated.View style={animationStyle}>
+            <Star filled={x <= this.state.rating ? true : false} />
+          </Animated.View>
+        </TouchableWithoutFeedback>
+      )
+    }
     return (
       <View style={styles.container}>
-        <Star />
+        <View style={{ flexDirection: 'row' }}>
+          {stars}
+        </View>
       </View>
     );
   }
@@ -17,7 +71,7 @@ class Star extends React.Component {
     return (
       <FontAwesome
       style={{ marginHorizontal: 6 }}
-        name='star'
+        name={this.props.filled === true ? 'star' : 'star-o'}
         size={32}
         color='red'
       />
