2bb986ed22fbfc12336a8042be9c49ce51453d7e
gneto <glaudistonson@gmail.com>
2021-04-06 16:27:21 -0300

    creating satars

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Star />
+      </View>
+    );
+  }
+}
+
+class Star extends React.Component {
+  render() {
+    return (
+      <FontAwesome
+      style={{ marginHorizontal: 6 }}
+        name='star'
+        size={32}
+        color='red'
+      />
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
